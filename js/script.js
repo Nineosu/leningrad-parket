0f8b347803ba886dce7011772f805c282ad0209d
nineosu <nineosu@gmail.com>
2024-06-18 04:12:53 +0300

    all

---
+++ js/script.js
@@ -0,0 +1,59 @@
+window.addEventListener('DOMContentLoaded', () => {
+    Fancybox.bind("[data-fancybox]", {
+        // Your custom options
+    });
+
+    const uploadContainer = document.getElementById('upload-container');
+    const fileInput = document.getElementById('file-input');
+    let fileCount = 0;
+
+    uploadContainer.addEventListener('click', () => {
+        fileInput.click();
+    });
+
+    uploadContainer.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        uploadContainer.classList.add('dragover');
+    });
+
+    uploadContainer.addEventListener('dragleave', () => {
+        uploadContainer.classList.remove('dragover');
+    });
+
+    uploadContainer.addEventListener('drop', (e) => {
+        e.preventDefault();
+        uploadContainer.classList.remove('dragover');
+        const files = Array.from(e.dataTransfer.files);
+        handleFiles(files);
+    });
+
+    fileInput.addEventListener('change', (e) => {
+        const files = Array.from(e.target.files);
+        handleFiles(files);
+    });
+
+    function handleFiles(files) {
+        if (fileCount + files.length > 5) {
+            alert('Вы можете загрузить до 5 фото.');
+            return;
+        }
+
+        fileCount += files.length;
+        uploadContainer.textContent = `Загружено фото: ${fileCount}`;
+    }
+
+    // burger
+    const burgerBtn = document.querySelector('.burger-btn'),
+          burgerMenu = document.querySelector('.header__nav-menu'),
+          closeBtn = document.querySelector('.close');
+
+
+    burgerBtn.addEventListener('click', () => {
+        burgerMenu.classList.add('show');
+        document.body.style.overflow = 'hidden';
+    });
+    closeBtn.addEventListener('click', () => {
+        burgerMenu.classList.remove('show');
+        document.body.style.overflow = '';
+    });
+});
